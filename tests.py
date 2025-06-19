d7516b2189a7719b8942c0b064cf77de5b87039e
kavancamp <keenahvancampenhout@gmail.com>
2025-06-19 14:20:59 -0500

    added more tests

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 
-from functions.get_file_content import get_files_content
-from functions.write_file import write_file
+# from functions.get_file_content import get_files_content
+# from functions.write_file import write_file
+from functions.run_python import run_python_file
+
 def tests():
     # #  Run get_files_info("calculator", ".")
    
@@ -15,14 +17,29 @@ def tests():
 
     # # write file tests
     
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # print(result)
+    
+    # result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    # print(result)
+    
+    # result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    # print(result)
+    
+    result = run_python_file("calculator", "main.py")
     print(result)
     
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "tests.py")
     print(result)
     
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(result)
+    result = run_python_file("calculator", "../main.py") 
+    print(result)#this should return an error 
+    
+    result = run_python_file("calculator", "nonexistent.py") 
+    print(result)#this should return an error
+    
+    result = run_python_file("calculator", "new.go") 
+    print(result)#this should return an error
     
 if __name__ == "__main__":
     tests()
